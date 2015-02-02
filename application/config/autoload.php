e20ea86d45ff04bbc68b845c3c829a353d83f523
AdanCruzHuerta <adancruzhuerta@gmail.com>
2015-02-02 12:15:18 -0600

    primer commit

---
+++ application/config/autoload.php
@@ -0,0 +1,47 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$autoload['packages'] = array();
+
+
+/*
+| -------------------------------------------------------------------
+|  Auto-load Libraries
+| -------------------------------------------------------------------
+| These are the classes located in the system/libraries folder
+| or in your application/libraries folder.
+|
+| Prototype:
+|
+|	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
+*/
+
+$autoload['libraries'] = array('database');
+
+
+/*
+| -------------------------------------------------------------------
+|  Auto-load Helper Files
+| -------------------------------------------------------------------
+| Prototype:
+|
+|	$autoload['helper'] = array('url', 'file');
+*/
+
+$autoload['helper'] = array('url');
+
+$autoload['config'] = array();
+
+$autoload['language'] = array();
+
+
+/*
+| -------------------------------------------------------------------
+|  Auto-load Models
+| -------------------------------------------------------------------
+| Prototype:
+|
+|	$autoload['model'] = array('model1', 'model2');
+|
+*/
+
+$autoload['model'] = array();
---
+++ application/config/config.php
@@ -0,0 +1,97 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Base Site URL
+|--------------------------------------------------------------------------
+|
+| URL to your CodeIgniter root. Typically this will be your base URL,
+| WITH a trailing slash:
+|
+|	http://example.com/
+|
+| If this is not set then CodeIgniter will guess the protocol, domain and
+| path to your installation.
+|
+*/
+$config['base_url']	= 'http://localhost/contexto/';
+
+/*
+|--------------------------------------------------------------------------
+*/
+
+$config['index_page'] = 'index.php';
+
+$config['uri_protocol']	= 'AUTO';
+
+$config['url_suffix'] = '';
+
+$config['language']	= 'english';
+
+$config['charset'] = 'UTF-8';
+
+$config['enable_hooks'] = FALSE;
+
+$config['subclass_prefix'] = 'MY_';
+
+$config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-';
+
+$config['allow_get_array']		= TRUE;
+$config['enable_query_strings'] = FALSE;
+$config['controller_trigger']	= 'c';
+$config['function_trigger']		= 'm';
+$config['directory_trigger']	= 'd'; // experimental not currently in use
+
+$config['log_threshold'] = 0;
+
+$config['log_path'] = '';
+
+$config['log_date_format'] = 'Y-m-d H:i:s';
+
+$config['cache_path'] = '';
+
+/*
+|--------------------------------------------------------------------------
+| Encryption Key
+|--------------------------------------------------------------------------
+|
+| If you use the Encryption class or the Session class you
+| MUST set an encryption key.  See the user guide for info.
+|
+*/
+$config['encryption_key'] = '.contexto.Colima.';
+
+/*
+|--------------------------------------------------------------------------
+*/
+
+$config['sess_cookie_name']		= 'ci_session';
+$config['sess_expiration']		= 7200;
+$config['sess_expire_on_close']	= FALSE;
+$config['sess_encrypt_cookie']	= FALSE;
+$config['sess_use_database']	= FALSE;
+$config['sess_table_name']		= 'ci_sessions';
+$config['sess_match_ip']		= FALSE;
+$config['sess_match_useragent']	= TRUE;
+$config['sess_time_to_update']	= 300;
+
+$config['cookie_prefix']	= "";
+$config['cookie_domain']	= "";
+$config['cookie_path']		= "/";
+$config['cookie_secure']	= FALSE;
+
+$config['global_xss_filtering'] = FALSE;
+
+$config['csrf_protection'] = FALSE;
+$config['csrf_token_name'] = 'csrf_test_name';
+$config['csrf_cookie_name'] = 'csrf_cookie_name';
+$config['csrf_expire'] = 7200;
+
+
+$config['compress_output'] = FALSE;
+
+$config['time_reference'] = 'local';
+
+$config['rewrite_short_tags'] = FALSE;
+
+$config['proxy_ips'] = '';
